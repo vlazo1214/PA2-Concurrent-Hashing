de0eba74aec3bc3d33b5ec732d5956ca9f763f60
vlazo1214 <vincentl1214@yahoo.com>
2024-04-16 19:38:05 -0400

    implemented rwlocks

---
+++ rwlocks.c
@@ -0,0 +1,63 @@
+// file implementing read/ write locks
+
+#include "rwlocks.h"
+
+void rwlock_init(rwlock_t *lock) {
+    lock->readers = 0;
+    Sem_init(&lock->lock, 1); 
+    Sem_init(&lock->writelock, 1); 
+}
+
+void rwlock_acquire_readlock(rwlock_t *lock) {
+    Sem_wait(&lock->lock);
+    lock->readers++;
+    if (lock->readers == 1)
+	Sem_wait(&lock->writelock);
+    Sem_post(&lock->lock);
+}
+
+void rwlock_release_readlock(rwlock_t *lock) {
+    Sem_wait(&lock->lock);
+    lock->readers--;
+    if (lock->readers == 0)
+	Sem_post(&lock->writelock);
+    Sem_post(&lock->lock);
+}
+
+void rwlock_acquire_writelock(rwlock_t *lock) {
+    Sem_wait(&lock->writelock);
+}
+
+void rwlock_release_writelock(rwlock_t *lock) {
+    Sem_post(&lock->writelock);
+}
+
+int read_loops;
+int write_loops;
+int counter = 0;
+
+rwlock_t mutex;
+
+void *reader(void *arg) {
+    int i;
+    int local = 0;
+    for (i = 0; i < read_loops; i++) {
+	rwlock_acquire_readlock(&mutex);
+	local = counter;
+	rwlock_release_readlock(&mutex);
+	printf("read %d\n", local);
+    }
+    printf("read done: %d\n", local);
+    return NULL;
+}
+
+void *writer(void *arg) {
+    int i;
+    for (i = 0; i < write_loops; i++) {
+	rwlock_acquire_writelock(&mutex);
+	counter++;
+	rwlock_release_writelock(&mutex);
+    }
+    printf("write done\n");
+    return NULL;
+}
