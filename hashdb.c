cd3c34cfde7710626165d20f6c819a430a2e6be4
dawnmartin30 <dawntwi30@gmail.com>
2024-04-19 13:40:44 -0400

    fix hash

--- hashdb.c
+++ hashdb.c
@@ -1,18 +1,24 @@
 #include "hashdb.h"
+#include <stdio.h>
 
-uint32_t calculate_hash(const char *str) {
-    uint32_t hash = 5381;
-
-    while (*str) {
-        hash = ((hash << 5) + hash) + *str++; 
+uint32_t jenkins_one_at_a_time_hash(const uint8_t* key, size_t length) {
+    size_t i = 0;
+    uint32_t hash = 0;
+    while (i != length) {
+    hash += key[i++];
+    hash += hash << 10;
+    hash ^= hash >> 6;
     }
-
+    hash += hash << 3;
+    hash ^= hash >> 11;
+    hash += hash << 15;
     return hash;
 }
 
 bool insert(hashRecord *hashTable[], const char *name, uint32_t salary) {
     // Calculate hash value for the given name
-    uint32_t hash = calculate_hash(name);
+    uint32_t hash = jenkins_one_at_a_time_hash((const uint8_t*)name, strlen(name));
+    printf("debug %s, %u\n", name, hash);
 
     // Create new record
     hashRecord *newRecord = malloc(sizeof(hashRecord));
